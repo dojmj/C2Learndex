0bcd1491a9d68b7e9a692f3e230faf122341f5d9
dojmj <joahae@ewhain.net>
2026-04-21 17:30:55 +0900

    Initial Commit

---
+++ Learndex/LearndexApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearndexApp.swift
+//  Learndex
+//
+//  Created by 조민지 on 4/21/26.
+//
+
+import SwiftUI
+
+@main
+struct LearndexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LearndexTests/LearndexTests.swift
@@ -0,0 +1,19 @@
+//
+//  LearndexTests.swift
+//  LearndexTests
+//
+//  Created by 조민지 on 4/21/26.
+//
+
+import Testing
+@testable import Learndex
+
+struct LearndexTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
